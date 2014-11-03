7cb77f98d6e539592dbad6e4404c7348dac8c8cf
Adam <daddatv@live.se>
2014-11-03 17:16:12 +0100

    Started working on a testing environment

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 80f;
+	public float maxSpeed = 9f;
+	public float jumpForce = 350f;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		HandleInput();
+	}
+
+	private void HandleInput()
+	{
+		var velX = rigidbody2D.velocity.x;
+		var velY = rigidbody2D.velocity.y;
+
+		if (rigidbody2D.velocity.x < maxSpeed)
+		{
+			if (Input.GetKey(KeyCode.D))
+			{
+				rigidbody2D.AddForce(Vector2.right * speed * Input.GetAxis("Horizontal"));
+			}
+		}
+
+		if (rigidbody2D.velocity.x > -maxSpeed)
+		{
+			if (Input.GetKey(KeyCode.A))
+			{
+				rigidbody2D.AddForce(Vector2.right * speed * Input.GetAxis("Horizontal"));
+			} 
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			rigidbody2D.AddForce(Vector2.up * jumpForce);
+		}
+
+	}
+}
