9b55595654cbac054b4e73a6eadb76c2faa198af
Adam <daddatv@live.se>
2014-11-06 15:55:04 +0100

    work on lerp n stuff

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehavior : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        var camPos = transform.position;
+        var camPosX = transform.position.x;
+        var camPosY = transform.position.y;
+        var playerPosX = player.transform.position.x;
+        var playerPosY = player.transform.position.y;
+
+        while (camPosX != playerPosX)
+        {
+            camPos = new Vector3(Mathf.Lerp(camPosX, playerPosX, 1f), camPosY, transform.position.z);
+        }
+	}
+}
