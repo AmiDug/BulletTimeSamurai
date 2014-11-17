ed35d1a2b55d89db3867ba7ff320a4c8658c6680
Adam <daddatv@live.se>
2014-11-17 14:31:33 +0100

    update
    cameras and stuff

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -1,28 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraBehavior : MonoBehaviour {
+public class CameraBehavior : MonoBehaviour
+{
 
     public GameObject player;
+    public float X = 0.05f;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
-    {
-        var camPos = transform.position;
-        var camPosX = transform.position.x;
-        var camPosY = transform.position.y;
         var playerPosX = player.transform.position.x;
         var playerPosY = player.transform.position.y;
 
-        while (camPosX != playerPosX)
-        {
-            camPos = new Vector3(Mathf.Lerp(camPosX, playerPosX, 1f), camPosY, transform.position.z);
-        }
-	}
+        this.transform.localPosition = new Vector3(playerPosX, playerPosY, this.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCam(player.transform.localPosition.x, player.transform.localPosition.y);
+    }
+
+    public void MoveCam(float posX, float posY)
+    {
+        float camPosX = transform.localPosition.x;
+        float camPosY = transform.localPosition.y;
+
+        float offsetY = camPosY - posY;
+
+        this.transform.localPosition = new Vector3(Mathf.Lerp(camPosX, posX, X), (camPosY - offsetY) * 0.55f, transform.localPosition.z);
+    }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,49 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	public float speed = 80f;
-	public float maxSpeed = 9f;
-	public float jumpForce = 350f;
+    public float speed = 15f;
+    public float maxSpeed = 8f;
+    public bool onGround = false;
+    public float jumpForce = 350f;
 
-	// Use this for initialization
-	void Start ()
-	{
+    // Use this for initialization
+    void Start()
+    {
 
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		HandleInput();
-	}
+    }
 
-	private void HandleInput()
-	{
-		var velX = rigidbody2D.velocity.x;
-		var velY = rigidbody2D.velocity.y;
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+        Debug.Log(Input.GetAxis("Horizontal"));
+    }
 
-		if (rigidbody2D.velocity.x < maxSpeed)
-		{
-			if (Input.GetKey(KeyCode.D))
-			{
-				rigidbody2D.AddForce(Vector2.right * speed * Input.GetAxis("Horizontal"));
-			}
-		}
+    private void HandleInput()
+    {
+        var velX = rigidbody2D.velocity.x;
+        var velY = rigidbody2D.velocity.y;
+        float z = 0f;
 
-		if (rigidbody2D.velocity.x > -maxSpeed)
-		{
-			if (Input.GetKey(KeyCode.A))
-			{
-				rigidbody2D.AddForce(Vector2.right * speed * Input.GetAxis("Horizontal"));
-			} 
-		}
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            if (Mathf.Abs(rigidbody2D.velocity.x) < maxSpeed)
+            {
+                //rigidbody2D.AddForce(Vector2.right * speed * Input.GetAxisRaw("Horizontal"));
+				rigidbody2D.velocity = new Vector3(speed * Input.GetAxis("Horizontal"), rigidbody2D.velocity.y, z);
+            }
+        }
+		//else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+		//{
+		//	rigidbody2D.velocity = new Vector3(0f, rigidbody2D.velocity.y, z);
+		//}
 
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			rigidbody2D.AddForce(Vector2.up * jumpForce);
-		}
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
 
-	}
+        if (rigidbody2D.velocity.y == 0)
+        {
+            onGround = true;
+        }else
+        {
+            onGround = false;
+        }
+    }
+
+    void Jump()
+    {
+        if (onGround == true)
+        {
+            rigidbody2D.AddForce(Vector2.up * jumpForce);
+        }
+    }
 }
