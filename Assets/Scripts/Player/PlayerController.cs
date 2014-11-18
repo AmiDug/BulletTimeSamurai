6a6ee39516e7863a47c130194731f2e8a26e8d9c
Adam <daddatv@live.se>
2014-11-18 17:06:13 +0100

    x

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,62 +4,49 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 15f;
-    public float maxSpeed = 8f;
-    public bool onGround = false;
-    public float jumpForce = 350f;
+	public float speed = 6f;
+	//public float maxSpeed = 8f;
+	public bool onGround = false;
+	public float jumpForce = 350f;
 
-    // Use this for initialization
-    void Start()
-    {
+	// Use this for initialization
+	void Start()
+	{
 
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        HandleInput();
-        Debug.Log(Input.GetAxis("Horizontal"));
-    }
+	// Update is called once per frame
+	void FixedUpdate() // R U SRS IT WAS SERIOUSLY ONLY TO ENABLE INTERPOLATION
+	{
+		HandleInput();
+	}
 
-    private void HandleInput()
-    {
-        var velX = rigidbody2D.velocity.x;
-        var velY = rigidbody2D.velocity.y;
-        float z = 0f;
+	private void HandleInput()
+	{
 
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
-        {
-            if (Mathf.Abs(rigidbody2D.velocity.x) < maxSpeed)
-            {
-                //rigidbody2D.AddForce(Vector2.right * speed * Input.GetAxisRaw("Horizontal"));
-				rigidbody2D.velocity = new Vector3(speed * Input.GetAxis("Horizontal"), rigidbody2D.velocity.y, z);
-            }
-        }
-		//else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
-		//{
-		//	rigidbody2D.velocity = new Vector3(0f, rigidbody2D.velocity.y, z);
-		//}
+		rigidbody2D.velocity = new Vector3(speed * Input.GetAxisRaw("Horizontal"), rigidbody2D.velocity.y, 0f);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Jump();
-        }
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Jump();
+		}
+	}
 
-        if (rigidbody2D.velocity.y == 0)
-        {
-            onGround = true;
-        }else
-        {
-            onGround = false;
-        }
-    }
+	void Jump()
+	{
+		if (onGround == true)
+		{
+			rigidbody2D.AddForce(Vector2.up * jumpForce);
+		}
+	}
 
-    void Jump()
-    {
-        if (onGround == true)
-        {
-            rigidbody2D.AddForce(Vector2.up * jumpForce);
-        }
-    }
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		onGround = true;
+	}
+
+	void OnTriggerExit2D(Collider2D coll)
+	{
+		onGround = false;
+	}
 }
