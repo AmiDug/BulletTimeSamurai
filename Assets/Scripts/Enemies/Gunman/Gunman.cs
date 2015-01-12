b8f1c6cfd01ee0d7408464e18b5510b79d22e731
Adam <daddatv@live.se>
2015-01-12 15:47:21 +0100

    Gunman work
    • Added trigger to laser
    • Added code to make him shoot [Doesn't work]

--- Assets/Scripts/Enemies/Gunman/Gunman.cs
+++ Assets/Scripts/Enemies/Gunman/Gunman.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using UnityEngine;
+using System.Collections;
 
-namespace Assets.Scripts.Enemies.Gunman
+public class Gunman : MonoBehaviour
 {
-	class Gunman
+
+	Vector3 bulletStartPostition;
+	public GameObject bullet;
+	Rigidbody2D bulletRB;
+	BulletTime bulletTime;
+
+	void Start()
 	{
-		
+		bulletStartPostition = GameObject.Find("BulletStartPosition").transform.position;
+		bulletTime = GameObject.FindGameObjectWithTag("Player").GetComponent<BulletTime>();
+	}
+
+	void Update()
+	{
+
+	}
+
+	public void Shoot()
+	{
+		GameObject firedBullet = GameObject.Instantiate(bullet, bulletStartPostition, new Quaternion(0f, 0f, 0f, 0f)) as GameObject;
+		bulletRB = firedBullet.GetComponent<Rigidbody2D>();
+		bulletRB.AddForce((Vector2.right * -bulletTime.bulletSpeed) * transform.localScale.x);
 	}
 }
---
+++ Assets/Scripts/Enemies/Gunman/LaserHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserHandler : MonoBehaviour
+{
+
+	Gunman gunman;
+
+	void Start()
+	{
+		gunman = this.GetComponentInParent<Gunman>();
+	}
+
+	void Update()
+	{
+
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		Debug.Log("Found ya");
+		gunman.Shoot();
+	}
+}
