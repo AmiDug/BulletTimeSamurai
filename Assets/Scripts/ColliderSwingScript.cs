f14e05b21a13b2794e43b02e4e9fb25ebe784c03
Bansheebomb <dugiev123@gmail.com>
2015-02-09 14:40:25 +0100

    Bigger map, test-script
    I'm trying to make a script that makes it so that if the swing particle
    touches the fenceturret collider, the collider is deactivated. failing
    so far.

---
+++ Assets/Scripts/ColliderSwingScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderSwingScript : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		//collider = this.GetComponent<BoxCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void OnTriggerEnter(Collider2D other)
+	    {
+		if(other.gameObject.tag == "swing")
+		   {
+			collider.isTrigger = true;
+		   }
+		}
+}
